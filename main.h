2e4b97f5cdb3fc152a77dfef55b572510e662a10
nvzree <davidnazree@gmail.com>
2023-09-19 12:50:20 -0700

    main.h

--- main.h
+++ main.h
@@ -9,7 +9,6 @@
 int _putchar(char c);
 int _putnum(long num, int count);
 int (*get_func(char str))(va_list arg);
-int get_precision(const char *format, int *i, va_list list);
 
 
 /* printf functions */
@@ -17,6 +16,5 @@ int _printf(const char *format, ...);
 int print_c(va_list args);
 int print_s(va_list args);
 int print_d(va_list args);
-int get_flags(const char *format, int *i);
 
 #endif
