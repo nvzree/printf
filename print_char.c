bee3add12266302a0763b3b5f1a3e154a6de899f
malvinnthapeleng <malvinnthapeleng@gmail.com>
2023-09-19 18:48:07 -0700

    updates

--- print_char.c
+++ print_char.c
@@ -11,14 +11,15 @@ int print_c(va_list args)
 {
 	int count = 0;
 	char c = va_arg(args, int);
-
+/*Check if the character is null*/
 	if (c == 0)
 	{
+		/*If yes, change it to the null character '\0'*/
 		c = '\0';
 	}
 
 	_putchar(c);
 	count++;
-
+/*Return the total number of characters printed*/
 	return (count);
 }
